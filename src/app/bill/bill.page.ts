2763b660be8b297d086707d3964fbafb6d5d1305
saksham raj thakur <thakursaksham252@gmail.com>
2023-06-19 20:35:32 +0530

    fourth commit

--- src/app/bill/bill.page.ts
+++ src/app/bill/bill.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource , MatTableModule} from '@angular/material/table';
+interface MyObject {
+  itemCode:string, 
+  itemName:string, 
+  price:string, 
+  quantity:string, 
+  totalPrice:number;
+}
 
 @Component({
   selector: 'app-bill',
@@ -7,22 +14,119 @@ import { MatTableDataSource , MatTableModule} from '@angular/material/table';
   styleUrls: ['./bill.page.scss'],
 })
 export class BillPage implements OnInit {
-  public data = new MatTableDataSource<any>()
-  addq = [{}]
-  
-  displayedColumns: string[] = ['code', 'itemName', 'price', 'quantity'];
 
-  constructor() { }
+
+  phoneNumber: string;
+  name: string;
+  billNumber: string;
+  items: any[]; // Replace with your actual data structure
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ['itemCode', 'itemName', 'price', 'quantity', 'totalPrice'];
+  totalItems: number;
+  totalQuantity: number;
+  totalAmount: number;
+  paymentMethod: string;
+  isAddButtonClicked: boolean;
+  itemcode:string;
+  itemquandity:string;
+  tableData :MyObject[]=[]
+  
+
+  constructor() { 
+
+    this.phoneNumber = '';
+    this.name = '';
+    this.billNumber = '';
+    this.items = []; // Initialize with your data
+    this.dataSource = new MatTableDataSource(this.tableData);
+
+    this.totalItems = 0;
+    this.totalQuantity = 0;
+    this.totalAmount = 0;
+    this.paymentMethod = '';
+    this.isAddButtonClicked =false;
+    this.itemcode='';
+    this.itemquandity='';
+  }
 
   ngOnInit() {
   }
 
-  add(){
-    this.addq.push( { code: '001', itemName: 'Item 1', price: 10, quantity: 5 })
-    this.addq.push( { code: '001', itemName: 'Item 1', price: 10, quantity: 5 })
-    this.addq.push( { code: '001', itemName: 'Item 1', price: 10, quantity: 5 })
-    this.addq.push( { code: '001', itemName: 'Item 1', price: 10, quantity: 5 })
-    this.data= new MatTableDataSource(this.addq)
-  } 
+  
+
+  changeQuantity(item: any) {
+    // Logic to change quantity for the selected item
+    console.log(this.tableData)
+    console.log(item['quantity'] , item['price'],item['quantity'] * item['price'])
+    item['totalPrice'] = item['quantity'] * item['price']
+    console.log(item)
+    console.log(this.tableData)
+    this.total_fun()
+  }
+
+  addItem() {
+    // Logic to add an item to the list
+    this.isAddButtonClicked =!this.isAddButtonClicked;
+    if (parseInt(this.itemcode)<=10000 && this.isAddButtonClicked==false){
+      this.tableData.push( { itemCode: this.itemcode, itemName: 'Manual', price: this.itemcode, quantity: this.itemquandity,totalPrice:parseInt(this.itemquandity)*parseInt(this.itemcode)  })
+      this.dataSource = new MatTableDataSource(this.tableData)
+      this.itemcode='';
+      this.itemquandity='';
+      this.total_fun()
+    }
+    console.log(this.tableData)
+  }
+
+  stashItems() {
+    // Logic to stash items
+  }
+
+  updateItems() {
+    // Logic to update items
+  }
+
+  clearItems() {
+    this.phoneNumber = '';
+    this.name = '';
+    this.billNumber = '';
+    this.items = []; // Initialize with your data
+    this.tableData=[]
+    this.dataSource = new MatTableDataSource(this.tableData);
+
+    this.totalItems = 0;
+    this.totalQuantity = 0;
+    this.totalAmount = 0;
+    this.paymentMethod = '';
+    this.isAddButtonClicked =false;
+    this.itemcode='';
+    this.itemquandity='';
+  
+  }
+  total_fun(){
+    this.totalItems = this.tableData.length
+    
+    // for (const item of this.tableData) {
+    //   this.totalQuantity = item['quantity']
+    // }
+    console.log('im')
+    this.tableData.forEach((item) => {
+      this.totalQuantity = this.totalQuantity +  parseInt( item.quantity);
+      this.totalAmount = this.totalAmount + item.totalPrice;
+    })
+  }
+
+  generateBill() {
+    // Logic to generate the bill based on the selected payment method
+  }
 
 }
+
+
+
+
+
+
+
+
+
+
